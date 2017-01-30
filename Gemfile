5ccf79fc57e5d34e34baafa474a98ddd24d25775
vscalzo000 <vscalzo000@gmail.com>
2017-01-30 09:25:56 -0500

    Setup and devise

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'dotenv'
+gem 'devise'
 
 group :test do
   gem 'coveralls', require: false
---
+++ db/migrate/20170130140347_devise_create_users.rb
@@ -0,0 +1,18 @@
+class DeviseCreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      ## Extra
+      t.string :username,               null: false, default: ''
+      ## Database authenticatable
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email,                unique: true
+  end
+end
